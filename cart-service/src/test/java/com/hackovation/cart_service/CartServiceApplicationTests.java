e28c980eb2eea2957a8ac1133f901105d64a91fe
Sabareesh S <ssabareesh646@gmail.com>
2025-08-09 18:24:23 +0530

    Add cart service

---
+++ cart-service/src/test/java/com/hackovation/cart_service/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackovation.cart_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
